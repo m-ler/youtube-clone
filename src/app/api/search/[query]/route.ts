d461f7377bef47f58f3224877a688a042b27fd88
miguelangellerma <miguellangel770@gmail.com>
2023-07-13 21:59:50 -0500

    Added search results api route

---
+++ src/app/api/search/[query]/route.ts
@@ -0,0 +1,35 @@
+import { youtubeClient } from '@/lib/googleapis/youtube-client'
+import { NextRequest, NextResponse } from 'next/server'
+
+type Params = {
+	params: {
+		query: string
+	}
+}
+
+export const GET = async (req: NextRequest, { params }: Params) => {
+	const { query } = params
+
+	if (!query) {
+		return new NextResponse('Invalid search query.', {
+			status: 400,
+		})
+	}
+
+	try {
+		const res = await youtubeClient.search.list({
+			part: ['snippet'],
+			maxResults: 50,
+			q: query,
+			type: ['video'],
+		})
+
+		console.log(res.data)
+
+		return NextResponse.json(res.data)
+	} catch (e) {
+		return new NextResponse("Couldn't fetch results.", {
+			status: 500,
+		})
+	}
+}
--- src/app/api/videos/route.ts
+++ src/app/api/videos/route.ts
@@ -2,16 +2,18 @@ import { youtubeClient } from '@/lib/googleapis/youtube-client'
 import { NextResponse } from 'next/server'
 
 export const GET = async () => {
-	const res = await youtubeClient.videos.list({
-		part: ['snippet,contentDetails,statistics'],
-		chart: 'mostPopular',
-		regionCode: 'US',
-		maxResults: 50,
-	})
+	try {
+		const res = await youtubeClient.videos.list({
+			part: ['snippet, contentDetails, statistics'],
+			chart: 'mostPopular',
+			regionCode: 'US',
+			maxResults: 50,
+		})
 
-	if (res && res.data) return NextResponse.json(res.data)
-
-	return new NextResponse("Couldn't fetch feed.", {
-		status: 500,
-	})
+		return NextResponse.json(res.data)
+	} catch (e) {
+		return new NextResponse("Couldn't fetch feed.", {
+			status: 500,
+		})
+	}
 }
