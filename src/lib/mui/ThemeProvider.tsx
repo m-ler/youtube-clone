83b63d9a51d24ab3245ffdd8bc485429aad028ca
miguelangellerma <miguellangel770@gmail.com>
2023-07-28 20:50:43 -0500

    Added MUI Emotion cache

--- src/lib/mui/ThemeProvider.tsx
+++ src/lib/mui/ThemeProvider.tsx
@@ -1,11 +1,16 @@
-'use client';
+'use client'
 
-import { ThemeProvider as MUIThemeProvider } from '@mui/material/styles';
-import theme from './theme';
-import { PropsWithChildren } from 'react';
+import { ThemeProvider as MUIThemeProvider } from '@mui/material/styles'
+import theme from './theme'
+import { PropsWithChildren } from 'react'
+import { NextAppDirEmotionCacheProvider } from './EmotionCache'
 
 const ThemeProvider = ({ children }: PropsWithChildren) => {
-	return <MUIThemeProvider theme={theme}>{children}</MUIThemeProvider>;
-};
+	return (
+		<NextAppDirEmotionCacheProvider options={{ key: 'mui' }}>
+			<MUIThemeProvider theme={theme}>{children}</MUIThemeProvider>;
+		</NextAppDirEmotionCacheProvider>
+	)
+}
 
-export default ThemeProvider;
+export default ThemeProvider
