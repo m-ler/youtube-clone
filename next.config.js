1bfd32bcbd46635a6268d06d5bd1ba93f6ee2c76
miguelangellerma <miguellangel770@gmail.com>
2023-07-29 20:10:16 -0500

    Added SVGR config in next.config.js

--- next.config.js
+++ next.config.js
@@ -3,6 +3,27 @@ const nextConfig = {
 	images: {
 		domains: ['i.ytimg.com'],
 	},
+	webpack(config) {
+		// SVGR config
+		const fileLoaderRule = config.module.rules.find((rule) => rule.test?.test?.('.svg'))
+
+		config.module.rules.push(
+			{
+				...fileLoaderRule,
+				test: /\.svg$/i,
+				resourceQuery: /url/, 
+			},
+			{
+				test: /\.svg$/i,
+				issuer: /\.[jt]sx?$/,
+				resourceQuery: { not: /url/ },
+				use: ['@svgr/webpack'],
+			}
+		)
+
+		fileLoaderRule.exclude = /\.svg$/i
+		return config
+	},
 }
 
 module.exports = nextConfig
