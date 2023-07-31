4cea7912bf692c6be743d1947dd7dca05456be19
miguelangellerma <miguellangel770@gmail.com>
2023-07-31 14:41:32 -0500

    Removed React Query

--- src/config/Providers.tsx
+++ src/config/Providers.tsx
@@ -2,14 +2,8 @@
 
 import ThemeProvider from '@/lib/mui/ThemeProvider'
 import { PropsWithChildren } from 'react'
-import { QueryClient, QueryClientProvider } from 'react-query'
-const queryClient = new QueryClient()
 const Providers = ({ children }: PropsWithChildren) => {
-	return (
-		<ThemeProvider>
-			<QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
-		</ThemeProvider>
-	)
+	return <ThemeProvider>{children}</ThemeProvider>
 }
 
 export default Providers
